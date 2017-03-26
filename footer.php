ecae375fd42b964d90fe7f5a22aee0be49350fc6
Gonzalo Ribera <kune@iMac-de-Gonzalo.local>
2017-03-26 20:06:02 +0200

    rrss footer

--- footer.php
+++ footer.php
@@ -1,34 +1,23 @@
-			</div>
-			<!-- /wrapper -->
-
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
-
-
-				<div class="clear"></div>
-
-				<!-- copyright -->
+		</div>
+		<!-- /wrapper -->
+		<!-- footer -->
+		<footer class="footer" role="contentinfo">
+			<ul class="social-links">
+				<li class="link-facebook"><a class="icon-facebook" href="https://www.facebook.com/mtblosmolinos" target="_blank"></a></li>
+				<li class="link-instagram"><a class="icon-instagram" href="https://www.instagram.com/mtblosmolinos/" target="_blank"></a></li>
+				<li class="link-mail"><a class="icon-mail" href="mailto:clubmtblosmolinos@gmail.com" target="_blank"></a></li>
+			</ul>
+			<div class="legal-footer">
 				<p class="copyright">
 					&copy; <?php echo date('Y'); ?> Derechos reservados <?php bloginfo('name'); ?>
 				</p>
-				<!-- /copyright -->
+				<p class="author" style="text-align: center;padding:0;margin:0;">Made with love by <a href="https://www.twitter.com/kune_" target="_blank" style="color:#E59333">@kune</a></p>
+			</div>
 
-			</footer>
-			<!-- /footer -->
-
-		</div>
+		</footer>
+		<!-- /footer -->
 
 		<?php wp_footer(); ?>
-
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
 
 	</body>
 </html>
--- front-page.php
+++ front-page.php
@@ -1,124 +1,123 @@
 <?php get_header(); ?>
 
 </div>
-	<section class="home-slider">
-		<?php $args = array(
-			'category_name' => 'destacado',
-			'order_by' => 'date',
-			'order' => 'DESC',
-			'post_per_page' => 3
-		); ?>
-
-		<?php $slider = new WP_Query($args); ?>
-		<ul class="slider">
-		<?php while($slider->have_posts()): $slider->the_post(); ?>
-			<li>
-				<a href="<?php the_permalink(); ?>">
-					<?php the_post_thumbnail('slider'); ?>
-					<div class="info-slider">
-						<h3><?php the_title(); ?></h3>
-						<?php html5wp_excerpt('html5wp_custom_post'); ?>
-					</div>
-				</a>
-			</li>
-		<?php endwhile; wp_reset_postdata(); ?>
-		</ul>
-	</section>
-
-	<section class="quehacemos clear">
-		<div class="wrapper">
-		<?php $args = array(
-			'post_type' => 'destacados',
-			'post_per_page' => 3,
-			'order' => 'ASC',
-			'orderby' => 'name'
-		) ?>
-
-		<?php $destacados = new WP_Query($args); ?>
-		<?php while($destacados->have_posts() ):$destacados->the_post(); ?>
-			<article id="post-<?php the_ID();?>" <?php post_class('grid1-3'); ?>>
-				<div class="imagen">
-					<?php the_post_thumbnail('iconoDestacado'); ?>
-				</div>
-				<div class="info-destacado">
-					<h5><?php the_title(); ?></h5>
-					<p><?php the_excerpt(); ?></p>
-				</div>
-				<div class="clear"></div>
-			</article>
-
-		<?php endwhile; wp_reset_postdata(); ?>
-		</div>
-	</section>
-	<div class="wrapper">
-
-		<section class="rutas-destacadas clear">
-			<h3 class="titular"><span>Rutas</span></h3>
-
+	<main role="main">
+		<section class="home-slider">
 			<?php $args = array(
-				'post_type' => 'rutas',
-				'post_per_page' => 3,
+				'category_name' => 'destacado',
+				'order_by' => 'date',
 				'order' => 'DESC',
-				'orderby' => 'name'
+				'post_per_page' => 3
 			); ?>
 
-			<?php $tours = new WP_Query($args); ?>
-			<?php while($tours->have_posts() ): $tours->the_post(); ?>
+			<?php $slider = new WP_Query($args); ?>
+			<ul class="slider">
+			<?php while($slider->have_posts()): $slider->the_post(); ?>
+				<li>
+					<a href="<?php the_permalink(); ?>">
+						<?php the_post_thumbnail('slider'); ?>
+						<div class="info-slider">
+							<h3><?php the_title(); ?></h3>
+							<?php html5wp_excerpt('html5wp_custom_post'); ?>
+						</div>
+					</a>
+				</li>
+			<?php endwhile; wp_reset_postdata(); ?>
+			</ul>
+		</section>
+
+		<section class="quehacemos clear">
+			<div class="wrapper">
+			<?php $args = array(
+				'post_type' => 'destacados',
+				'post_per_page' => 3,
+				'order' => 'ASC',
+				'orderby' => 'name'
+			) ?>
+
+			<?php $destacados = new WP_Query($args); ?>
+			<?php while($destacados->have_posts() ):$destacados->the_post(); ?>
 				<article id="post-<?php the_ID();?>" <?php post_class('grid1-3'); ?>>
-					<div class="imagen-destacada">
-						<?php the_post_thumbnail('rutaDestacada'); ?>
-						<a href="<?php the_permalink(); ?>" class="mas-info">
-							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masinfo.png" alt="">
-						</a>
+					<div class="imagen">
+						<?php the_post_thumbnail('iconoDestacado'); ?>
 					</div>
-					<div class="info-ruta">
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
-						</a>
+					<div class="info-destacado">
+						<h5><?php the_title(); ?></h5>
+						<p><?php the_excerpt(); ?></p>
 					</div>
 					<div class="clear"></div>
 				</article>
+
 			<?php endwhile; wp_reset_postdata(); ?>
-
-
-
+			</div>
 		</section>
-		<section class="ultimas-noticias clear">
-			<div class="grid2-3">
+		<div class="wrapper">
+
+			<section class="rutas-destacadas clear">
+				<h3 class="titular"><span>Rutas</span></h3>
+
 				<?php $args = array(
-					'post_type' => 'post',
+					'post_type' => 'rutas',
 					'post_per_page' => 3,
-					'order' => DESC,
-					'orderby' => 'date'
+					'order' => 'DESC',
+					'orderby' => 'name'
 				); ?>
 
-				<?php $ultimas = new WP_Query($args); ?>
-				<?php while($ultimas->have_posts() ): $ultimas->the_post(); ?>
-					<article class="entrada clear">
-						<div class="grid1-3">
-							<div class="foto">
-								<a href="<?php the_permalink(); ?>">
-									<?php the_post_thumbnail('mediano'); ?>
-								</a>
-							</div>
+				<?php $tours = new WP_Query($args); ?>
+				<?php while($tours->have_posts() ): $tours->the_post(); ?>
+					<article id="post-<?php the_ID();?>" <?php post_class('grid1-3'); ?>>
+						<div class="imagen-destacada">
+							<?php the_post_thumbnail('rutaDestacada'); ?>
+							<a href="<?php the_permalink(); ?>" class="mas-info">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masinfo.png" alt="">
+							</a>
 						</div>
-						<div class="grid2-3">
-							<h2>
-								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							</h2>
+						<div class="info-ruta">
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
 						</div>
-<<<<<<< HEAD
-						<div style="display:inline-block">
-=======
-						<div class="texto-ruta">
->>>>>>> eb57b1c566e2270f6fb4bd3bec5540a8834b43cc
-							<?php html5wp_excerpt('html5wp_custom_post') ?>
-						</div>
+						<div class="clear"></div>
 					</article>
 				<?php endwhile; wp_reset_postdata(); ?>
-			</div>
-			<div class="grid1-3">
-				<?php get_sidebar(); ?>
-			</div>
-		</section>
+
+
+
+			</section>
+			<section class="ultimas-noticias clear">
+				<div class="grid2-3">
+					<?php $args = array(
+						'post_type' => 'post',
+						'post_per_page' => 3,
+						'order' => DESC,
+						'orderby' => 'date'
+					); ?>
+
+					<?php $ultimas = new WP_Query($args); ?>
+					<?php while($ultimas->have_posts() ): $ultimas->the_post(); ?>
+						<article class="entrada clear">
+							<div class="grid1-3">
+								<div class="foto">
+									<a href="<?php the_permalink(); ?>">
+										<?php the_post_thumbnail('mediano'); ?>
+									</a>
+								</div>
+							</div>
+							<div class="grid2-3">
+								<h2>
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								</h2>
+							</div>
+							<div class="texto-ruta">
+								<?php html5wp_excerpt('html5wp_custom_post') ?>
+							</div>
+						</article>
+					<?php endwhile; wp_reset_postdata(); ?>
+				</div>
+				<div class="grid1-3">
+					<?php get_sidebar(); ?>
+				</div>
+			</section>
+		</div>
+	</main>
 <?php get_footer(); ?>
